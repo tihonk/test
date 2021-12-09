99dccdb0bd05c8f1c2f513dacce18b01614021be
Tihon Kasko <t.kasko@itransition.com>
2021-12-09 21:00:29 +0300

    Initial commit

---
+++ auth_service/src/egg/authentication/RegistrationFacade.java
@@ -0,0 +1,11 @@
+package egg.authentication;
+
+import egg.models.mainModels.UserModel;
+
+public class RegistrationFacade {
+
+    public boolean registerNewUser(UserModel user){
+        user.getId();
+        return true;
+    }
+}
---
+++ dynamic_models/src/egg/models/AccountParams.java
@@ -0,0 +1,17 @@
+package egg.models;
+
+public class AccountParams {
+
+    private Long id;
+
+    AccountParams() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
