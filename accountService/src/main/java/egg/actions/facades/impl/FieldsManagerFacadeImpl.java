50f394c85ba38c164658cefec43256f926e9350b
Tihon Kasko <t.kasko@itransition.com>
2021-12-20 17:11:32 +0300

    Brunch_3: Indirect dependency injection (via annotations)

--- accountService/src/main/java/egg/actions/facades/impl/FieldsManagerFacadeImpl.java
+++ accountService/src/main/java/egg/actions/facades/impl/FieldsManagerFacadeImpl.java
@@ -2,11 +2,19 @@ package egg.actions.facades.impl;
 
 import egg.actions.facades.FieldsManagerFacade;
 import egg.actions.services.FieldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class FieldsManagerFacadeImpl implements FieldsManagerFacade {
 
-    private FieldService fieldService;
+    private final FieldService fieldService;
+
+    @Autowired
+    public FieldsManagerFacadeImpl(FieldService fieldService){
+        super();
+        this.fieldService = fieldService;
+    }
 
     public void giveFreeField(Long userId) {
         fieldService.giveFreeField(userId);
